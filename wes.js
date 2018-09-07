0a170bfcdb837197d0d4b303141d9b5557b78e3f
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-09-07 19:23:23 +0300

    Update wes.js

--- wes.js
+++ wes.js
@@ -89,5 +89,28 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 }
 
 });
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "wsay") {
+
+   message.channel.sendMessage(args.join("  "))
+
+   message.delete()
+
+  }
+
+ });
+
+
 
 client.login(process.env.BOT_TOKEN);  //
